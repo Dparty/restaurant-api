796f3f27f0293ebd401be23c5aa6cd26ed7ca1ba
Dan Chen <dan.chen@boardware.com>
2023-12-04 15:31:52 +0800

    feat

--- controllers/bill.go
+++ controllers/bill.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/Dparty/common/fault"
 	"github.com/Dparty/common/utils"
@@ -9,6 +11,7 @@ import (
 	restaurantModels "github.com/Dparty/restaurant-services"
 	"github.com/chenyunda218/golambda"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
 type BillApi struct {
@@ -73,4 +76,114 @@ func (BillApi) CreateOrder(ctx *gin.Context) {
 		return
 	}
 	ctx.JSON(http.StatusCreated, BillBackward(*bill))
+}
+
+func (BillApi) BillSubscription(ctx *gin.Context) {
+	account := getAccount(ctx)
+	if account == nil {
+		return
+	}
+	restaurantId := ctx.Query("restaurantId")
+	restaurant, err := restaurantService.GetRestaurant(utils.StringToUint(restaurantId))
+	if err != nil {
+		fault.GinHandler(ctx, fault.ErrNotFound)
+		return
+	}
+	if account.ID() != restaurant.Owner().ID() {
+		fault.GinHandler(ctx, fault.ErrPermissionDenied)
+		return
+	}
+	tableId := ctx.Query("tableId")
+	status := ctx.Query("status")
+	startAt := ctx.Query("startAt")
+	endAt := ctx.Query("endAt")
+	var _tableId *uint
+	if tableId != "" {
+		_tableId = golambda.Reference(utils.StringToUint(tableId))
+	}
+	var _status *string
+	if status != "" {
+		_status = &status
+	}
+	var _startAt *time.Time
+	if startAt != "" {
+		_startAt = golambda.Reference(time.Unix(int64(utils.StringToUint(startAt)), 0))
+	}
+	var _endAt *time.Time
+	if endAt != "" {
+		_endAt = golambda.Reference(time.Unix(int64(utils.StringToUint(endAt)), 0))
+	}
+
+	conn, err := upgrader.Upgrade(ctx.Writer, ctx.Request, nil)
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+	mess := make(chan string)
+	go func() {
+		for {
+			_, _, err := conn.ReadMessage()
+			if err != nil {
+				close(mess)
+				break
+			}
+		}
+	}()
+	for {
+		select {
+		case _, ok := <-mess:
+			if !ok {
+				return
+			}
+		case <-time.After(time.Second * 3):
+			bills := golambda.Map(billService.ListBills(utils.StringToUint(restaurantId), _tableId, _status, _startAt, _endAt),
+				func(_ int, bill restaurantModels.Bill) apiModels.Bill {
+					return BillBackward(bill)
+				})
+			j, _ := json.Marshal(bills)
+			conn.WriteMessage(websocket.TextMessage, j)
+		}
+	}
+}
+
+func (BillApi) ListBills(ctx *gin.Context) {
+	account := getAccount(ctx)
+	if account == nil {
+		return
+	}
+	restaurantId := ctx.Query("restaurantId")
+	restaurant, err := restaurantService.GetRestaurant(utils.StringToUint(restaurantId))
+	if err != nil {
+		fault.GinHandler(ctx, fault.ErrNotFound)
+		return
+	}
+	if account.ID() != restaurant.Owner().ID() {
+		fault.GinHandler(ctx, fault.ErrPermissionDenied)
+		return
+	}
+	tableId := ctx.Query("tableId")
+	status := ctx.Query("status")
+	startAt := ctx.Query("startAt")
+	endAt := ctx.Query("endAt")
+	var _tableId *uint
+	if tableId != "" {
+		_tableId = golambda.Reference(utils.StringToUint(tableId))
+	}
+	var _status *string
+	if status != "" {
+		_status = &status
+	}
+	var _startAt *time.Time
+	if startAt != "" {
+		_startAt = golambda.Reference(time.Unix(int64(utils.StringToUint(startAt)), 0))
+	}
+	var _endAt *time.Time
+	if endAt != "" {
+		_endAt = golambda.Reference(time.Unix(int64(utils.StringToUint(endAt)), 0))
+	}
+	bills := billService.ListBills(utils.StringToUint(restaurantId), _tableId, _status, _startAt, _endAt)
+	ctx.JSON(http.StatusOK, golambda.Map(bills,
+		func(_ int, bill restaurantModels.Bill) apiModels.Bill {
+			return BillBackward(bill)
+		}))
 }
